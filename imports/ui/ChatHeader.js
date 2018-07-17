559324c74a9293ae1875e2d0e2b51a42fb46f208
Cody Boppert <cody@codyboppert.nyc>
2018-07-17 04:29:03 +0100

    Some basic styling

--- imports/ui/ChatHeader.js
+++ imports/ui/ChatHeader.js
@@ -3,9 +3,25 @@ import React, { Component } from 'react';
 export default class ChatHeader extends Component {
    render() {
       return(
-         <div id="chat-header">
-            <h1>Chat Window</h1>
-         </div>
+         <header id="chat-header">
+            <section id="chat-info">
+               <div id="chat-title">
+                  <h1>Chat Window</h1>
+               </div>
+               <div id="chat-actions">
+                  <h4 className="chat-action">Create Chat</h4>
+                  <h4 className="chat-action"> | Join Chat</h4>
+                  <h4 className="chat-action"> | Add Member to Chat</h4>
+               </div>
+            </section>
+            <section id="user-info">
+               <div id="avatar"></div>
+               <div id="user">
+                  <h1>Username</h1>
+                  <h4>Edit Profile</h4>
+               </div>
+            </section>
+         </header>
       );
    }
 }
