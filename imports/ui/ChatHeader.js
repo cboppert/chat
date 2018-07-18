ff335d498202e27270e50041c76199787fa6906f
Cody Boppert <cody@codyboppert.nyc>
2018-07-18 14:55:14 +0100

    Dont display chat list, chat title, or chat actions when not logged in

--- imports/ui/ChatHeader.js
+++ imports/ui/ChatHeader.js
@@ -6,16 +6,18 @@ export default class ChatHeader extends Component {
    render() {
       return(
          <header id="chat-header">
-            <section id="chat-info">
-               <div id="chat-title">
-                  <h1>Chat Window</h1>
-               </div>
-               <div id="chat-actions">
-                  <h4 className="chat-action">Create Chat</h4>
-                  <h4 className="chat-action"> | Join Chat</h4>
-                  <h4 className="chat-action"> | Add Member to Chat</h4>
-               </div>
-            </section>
+            { this.props.currentUser ?
+               <section id="chat-info">
+                  <div id="chat-title">
+                     <h1>Chat Window</h1>
+                  </div>
+                  <div id="chat-actions">
+                     <h4 className="chat-action">Create Chat</h4>
+                     <h4 className="chat-action"> | Join Chat</h4>
+                     <h4 className="chat-action"> | Add Member to Chat</h4>
+                  </div>
+               </section>
+            : <section id="chat-info"></section>}
             <section id="user-info">
                <div id="user">
                   <AccountsUIWrapper />
--- imports/ui/ChatList.js
+++ imports/ui/ChatList.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 
 export default class ChatList extends Component {
    render() {
-      return (
-         <section id="chat-list" class="sidebar-section">
+      return this.props.currentUser ?
+         (<section id="chat-list" class="sidebar-section">
             <h3>Chats</h3>
             <ul>
                <li>Chat One</li>
                <li>Chat Two</li>
             </ul>
-         </section>
-      );
+         </section>)
+         : (<section id="chat-list" class="sidebar-section">
+            <h3>Chats</h3>
+         </section>);
    }
 };
